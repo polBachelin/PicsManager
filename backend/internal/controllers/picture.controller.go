db3dc4c1f14619d094957214cbbc2c9bca7061a2
Pol Bachelin <pol.bachelin@epitech.eu>
2023-03-13 15:02:17 +0100

    update picture

--- backend/internal/controllers/picture.controller.go
+++ backend/internal/controllers/picture.controller.go
@@ -16,6 +16,10 @@ type PictureServiceController struct {
 	pbPicture.UnimplementedPictureServiceServer
 }
 
+func buildPictureMessage(pic models.Picture) *pbPicture.PictureMessage {
+	return &pbPicture.PictureMessage{AlbumId: pic.AlbumID.Hex(), OwnerName: pic.OwnerID.Hex(), Data: pic.Data, Name: pic.Name, ImageId: pic.ID.Hex(), Tags: pic.Tags}
+}
+
 func (s *PictureServiceController) CreatePicture(ctx context.Context, req *pbPicture.CreatePictureRequest) (*pbPicture.CreatePictureResponse, error) {
 	userID, err := GetIdFromContext(ctx)
 	if err != nil {
@@ -36,11 +40,29 @@ func (s *PictureServiceController) CreatePicture(ctx context.Context, req *pbPic
 	return &pbPicture.CreatePictureResponse{Pictures: &pbPicture.PictureMessage{AlbumId: picture.AlbumID.Hex(), OwnerName: picture.OwnerID.Hex(), Data: req.Data, Name: req.Name, ImageId: res.InsertedID.(primitive.ObjectID).Hex(), Tags: req.Tags}}, nil
 }
 
-func (s *PictureServiceController) UpdatePicture(ctx context.Context, in *pbPicture.UpdatePictureRequest) (*pbPicture.UpdatePictureResponse, error) {
-	return nil, nil
+func (s *PictureServiceController) UpdatePicture(ctx context.Context, req *pbPicture.UpdatePictureRequest) (*pbPicture.UpdatePictureResponse, error) {
+	userID, err := GetIdFromContext(ctx)
+	if err != nil {
+		log.Println("Error: ", err)
+		return nil, err
+	}
+	svc := services.NewPictureService()
+	var newPicture models.Picture
+	newPicture.AlbumID, err = primitive.ObjectIDFromHex(req.Pictures.AlbumId)
+	newPicture.ID, err = primitive.ObjectIDFromHex(req.Pictures.ImageId)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Could not convert picture id to ObjectID")
+
+	}
+	newPicture.OwnerID = userID
+	newPicture.Data = req.Pictures.Data
+	newPicture.Name = req.Pictures.Name
+	newPicture.Tags = req.Pictures.Tags
+	svc.UpdatePicture(newPicture)
+	return &pbPicture.UpdatePictureResponse{Pictures: buildPictureMessage(newPicture)}, nil
 }
 
-func (s *PictureServiceController) DeletePicture(ctx context.Context, in *pbPicture.DeletePictureRequest) (*pbPicture.DeletePictureResponse, error) {
+func (s *PictureServiceController) DeletePicture(ctx context.Context, req *pbPicture.DeletePictureRequest) (*pbPicture.DeletePictureResponse, error) {
 	return nil, nil
 }
 
@@ -52,10 +74,10 @@ func (s *PictureServiceController) ListPictures(req *pbPicture.ListPicturesReque
 	return nil
 }
 
-func (s *PictureServiceController) SearchPicturesByTag(ctx context.Context, in *pbPicture.SearchPicturesByTagRequest) (*pbPicture.SearchPicturesByTagResponse, error) {
+func (s *PictureServiceController) SearchPicturesByTag(ctx context.Context, req *pbPicture.SearchPicturesByTagRequest) (*pbPicture.SearchPicturesByTagResponse, error) {
 	return nil, nil
 }
 
-func (s *PictureServiceController) SearchPicturesByName(ctx context.Context, in *pbPicture.SearchPicturesByNameRequest) (*pbPicture.SearchPicturesByNameResponse, error) {
+func (s *PictureServiceController) SearchPicturesByName(ctx context.Context, req *pbPicture.SearchPicturesByNameRequest) (*pbPicture.SearchPicturesByNameResponse, error) {
 	return nil, nil
 }
--- backend/internal/services/picture.service.go
+++ backend/internal/services/picture.service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"picsManager/backend/internal/models"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -18,4 +19,25 @@ func (p Service) CreatePicture(obj models.Picture) (*mongo.InsertOneResult, erro
 	}
 	res, err := p.collection.InsertOne(context.TODO(), obj)
 	return res, err
+}
+
+func (p Service) GetPicture(id primitive.ObjectID) (models.Picture, error) {
+	var res models.Picture
+
+	cursor := p.collection.FindOne(context.TODO(), bson.M{"_id": id})
+	err := cursor.Decode(&res)
+	if err != nil {
+		return models.Picture{ID: primitive.ObjectID{0}, Name: "", OwnerID: primitive.ObjectID{0}}, err
+	}
+	return res, nil
+}
+
+func (p Service) DeletePicture(id primitive.ObjectID) (interface{}, error) {
+	res, err := p.collection.DeleteOne(context.TODO(), bson.M{"_id": id})
+	return res.DeletedCount, err
+}
+
+func (p Service) UpdatePicture(obj models.Picture) (*mongo.UpdateResult, error) {
+	update := bson.M{"$set": bson.M{"name": obj.Name, "data": obj.Data, "tags": obj.Tags, "albumId": obj.AlbumID, "ownerId": obj.OwnerID}}
+	return p.collection.UpdateOne(context.TODO(), bson.M{"_id": obj.ID}, update)
 }
