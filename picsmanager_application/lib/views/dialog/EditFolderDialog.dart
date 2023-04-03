e196bab5cc7952b44d33d336749f924f2a555e11
Boris <durand.boris@icloud.com>
2023-04-03 11:19:24 +0200

    feature search user

--- picsmanager_application/lib/views/dialog/EditFolderDialog.dart
+++ picsmanager_application/lib/views/dialog/EditFolderDialog.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:picsmanager_application/models/core/Album.dart';
 import 'package:picsmanager_application/models/core/Picture.dart';
 import 'package:picsmanager_application/models/core/User.dart';
+import 'package:picsmanager_application/protobuf/service/album_service.pb.dart';
 import 'package:picsmanager_application/providers/AuthenticationProvider.dart';
 import 'package:picsmanager_application/providers/InsertNewPictureForAlbum.dart';
 import 'package:picsmanager_application/providers/ListUserProvider.dart';
@@ -14,6 +15,8 @@ import 'package:provider/provider.dart';
 void EditFolderDialog({required BuildContext context, required Album album}) {
   AuthenticationProvider token =
       Provider.of<AuthenticationProvider>(context, listen: false);
+  ListUserProvider listUserProvider =
+  Provider.of<ListUserProvider>(context, listen: false);
   // PicturePageProvider picturePageProvider = Provider.of<PicturePageProvider>(context, listen: false);
   // picturePageProvider.startTrendingByAlbum(token.getToken, album);
   // InsertNewPictureForAlbum insertNewPictureForAlbum = Provider.of<InsertNewPictureForAlbum>(context);
@@ -32,87 +35,100 @@ void EditFolderDialog({required BuildContext context, required Album album}) {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(30.0),
               ),
-              child: Column(
-                children: [
-                  // Titre du dialog
-                  SizedBox(height: 10),
-                  Text("Album ${album.name}"),
-                  //Premier widget pour renommer le dossier
-                  Container(
-                    width: 300,
-                    height: 100,
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    alignment: Alignment.center,
-                    child: Column(
-                      children: [
-                        Text("renommer le dossier :"),
-                        TextFormField(
-                          controller: controller,
-                        )
-                      ],
-                    ),
-                  ),
-                  // Changer l'image du dossier
-                  // Row(
-                  //   children: [
-                  //     Selector<InsertNewPictureForAlbum, Picture>(
-                  //       selector: (_, provider,) => provider.picture,
-                  //       builder: (_, data, __) {
-                  //         return DropdownButton<String>(
-                  //           value: data.id,
-                  //             items: picturePageProvider.pictures
-                  //             .map<DropdownMenuItem<String>>((Picture picture) {
-                  //               return DropdownMenuItem<String>(
-                  //                 value: picture.id,
-                  //                   child: Text(picture.name),
-                  //               );
-                  //             }).toList(),
-                  //             onChanged: (String? value) {
-                  //               insertNewPictureForAlbum.picture.name = value!;
-                  //             },);
-                  //       },
-                  //     )
-                  //   ],
-                  // ),
-                  Container(
-                    width: 300,
-                    height: 200,
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    alignment: Alignment.center,
-                    child: Column(
-                      children: [
-                        Text("Partager avec : "),
-                        TextFormField(
-                          onChanged: (String value) {
-                            // Provider.of<ListUserProvider>(context)
-                            //     .startTrendingByName(
-                            //         token.getToken, value);
-                          },
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      // Titre du dialog
+                      SizedBox(height: 10),
+                      Text("Album ${album.name}"),
+                      //Premier widget pour renommer le dossier
+                      Container(
+                        width: 300,
+                        height: 100,
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        alignment: Alignment.center,
+                        child: Column(
+                          children: [
+                            Text("renommer le dossier :"),
+                            TextFormField(
+                              controller: controller,
+                            )
+                          ],
                         ),
-                         SingleChildScrollView(
-                            child: Selector<ListUserProvider, List<User>>(
-                                selector: (_, provider) => provider.users,
-                                builder: (_, data, __) {
-                                  return ListView(
-                                      children: data
-                                          .map((e) =>
-                                              ListTile(title: Text(e.email)))
-                                          .toList());
-                                }),
-                          ),
-                      ],
-                    ),
+                      ),
+                      // Changer l'image du dossier
+                      // Row(
+                      //   children: [
+                      //     Selector<InsertNewPictureForAlbum, Picture>(
+                      //       selector: (_, provider,) => provider.picture,
+                      //       builder: (_, data, __) {
+                      //         return DropdownButton<String>(
+                      //           value: data.id,
+                      //             items: picturePageProvider.pictures
+                      //             .map<DropdownMenuItem<String>>((Picture picture) {
+                      //               return DropdownMenuItem<String>(
+                      //                 value: picture.id,
+                      //                   child: Text(picture.name),
+                      //               );
+                      //             }).toList(),
+                      //             onChanged: (String? value) {
+                      //               insertNewPictureForAlbum.picture.name = value!;
+                      //             },);
+                      //       },
+                      //     )
+                      //   ],
+                      // ),
+                      Container(
+                        width: 300,
+                        height: 100,
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        alignment: Alignment.center,
+                        child: Column(
+                          children: [
+                            Text("Partager avec : "),
+                            TextFormField(
+                              onChanged: (String value) {
+                                print(value);
+                                listUserProvider.startTrendingByName(
+                                    token.getToken, value);
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 200,
+                        width: 300,
+                        alignment: Alignment.center,
+                        child: SingleChildScrollView(
+
+                          child: Selector<ListUserProvider, List<User>>(
+                              selector: (_, provider) => provider.users,
+                              builder: (_, data, __) {
+                                return ListView(
+                                    scrollDirection: Axis.vertical,
+                                    shrinkWrap: true,
+                                    children: data
+                                        .map((e) => ListTile(title: Text(e.email)))
+                                        .toList());
+                              }),
+                        ),
+                      ),
+                      OutlinedButton(
+                          onPressed: () async {
+                            await NetworkManager(token.getToken)
+                                .albumRepository
+                                .updateAlbum(album.id as AlbumMessage, controller.text, selected);
+                          },
+                          child: Text("save"))
+                    ],
                   ),
-                  OutlinedButton(
-                      onPressed: () async {
-                        await NetworkManager(token.getToken)
-                            .albumRepository
-                            .updateAlbum(album.raw, controller.text, selected);
-                        Navigator.pop(context, true);
-                      },
-                      child: Text("save"))
-                ],
-              ),
+                ),
+              )
             );
           },
         );
