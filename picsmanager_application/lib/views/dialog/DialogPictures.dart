911e6c677274bce318b73397dfbb5336cffbce29
Boris <durand.boris@icloud.com>
2023-04-03 14:27:55 +0200

    feature search user in image

--- picsmanager_application/lib/views/dialog/DialogPictures.dart
+++ picsmanager_application/lib/views/dialog/DialogPictures.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:picsmanager_application/models/core/User.dart';
 import 'package:picsmanager_application/protobuf/service/picture_service.pb.dart';
 import 'package:picsmanager_application/providers/AlbumProvider.dart';
 import 'package:picsmanager_application/providers/InsertAlbumProvider.dart';
+import 'package:picsmanager_application/providers/ListUserProvider.dart';
 import 'package:picsmanager_application/providers/PicturePageProvider.dart';
 import 'package:picsmanager_application/ressources/Network.dart';
 import 'package:provider/provider.dart';
@@ -29,7 +31,10 @@ Widget insertInAlbum(BuildContext context, String token, PictureMessage source) 
   InsertAlbumProvider insertAlbumProvider =
       Provider.of<InsertAlbumProvider>(context, listen: false);
   insertAlbumProvider.valueId = albumProvider.albums.first.id;
+  ListUserProvider listUserProvider =
+  Provider.of<ListUserProvider>(context, listen: false);
   PicturePageProvider provider = Provider.of<PicturePageProvider>(context, listen: false);
+  final controller = TextEditingController(text: source.name);
 
   return SizedBox(
       height: MediaQuery.of(context).size.height * 0.5,
@@ -79,7 +84,103 @@ Widget insertInAlbum(BuildContext context, String token, PictureMessage source) 
             await NetworkManager(token).pictureRepository.deletePicture(source.imageId);
             provider.deleteImageFromList(source.imageId);
             Navigator.pop(context, true);
-          }, child: Text("Delete Pictures"))
+          }, child: Text("Delete Pictures"),),
+      Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Titre du dialog
+              SizedBox(height: 10),
+              Text("Picture ${source.name}"),
+              //Premier widget pour renommer le dossier
+              Container(
+                width: 300,
+                height: 100,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    Text("renommer la photo :"),
+                    TextFormField(
+                      controller: controller,
+                    )
+                  ],
+                ),
+              ),
+              // Changer l'image du dossier
+              // Row(
+              //   children: [
+              //     Selector<InsertNewPictureForAlbum, Picture>(
+              //       selector: (_, provider,) => provider.picture,
+              //       builder: (_, data, __) {
+              //         return DropdownButton<String>(
+              //           value: data.id,
+              //             items: picturePageProvider.pictures
+              //             .map<DropdownMenuItem<String>>((Picture picture) {
+              //               return DropdownMenuItem<String>(
+              //                 value: picture.id,
+              //                   child: Text(picture.name),
+              //               );
+              //             }).toList(),
+              //             onChanged: (String? value) {
+              //               insertNewPictureForAlbum.picture.name = value!;
+              //             },);
+              //       },
+              //     )
+              //   ],
+              // ),
+              Container(
+                width: 300,
+                height: 100,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    Text("Partager avec : "),
+                    TextFormField(
+                      onChanged: (String value) {
+                        listUserProvider.startTrendingByName(
+                            token, value);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 200,
+                width: 300,
+                alignment: Alignment.center,
+                child: SingleChildScrollView(
+                  child: Selector<ListUserProvider, List<User>>(
+                      selector: (_, provider) => provider.users,
+                      shouldRebuild: (previous, next) => true,
+                      builder: (_, data, __) {
+                        return (data.isEmpty)
+                            ? Text("No result")
+                            : ListView(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            children: data
+                                .map((e) => ListTile(
+                                title: Text(e.email)))
+                                .toList());
+                      }),
+                ),
+              ),
+              OutlinedButton(
+                  onPressed: () async {
+                    await NetworkManager(token)
+                        .pictureRepository
+                        .setName(source.raw, controller.value.text);
+                  },
+                  child: Text("save"))
+            ],
+          ),
+        ),
+      ));
         ],
       ));
 }
